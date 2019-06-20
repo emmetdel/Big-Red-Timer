88870c8279569a0d7056213e45b4be4f04e9b323
Emmet Delaney <emmetdel@gmail.com>
2019-06-20 16:20:13 +0100

    play pause implemented

--- web/countdown_timer.dart
+++ web/countdown_timer.dart
@@ -1,49 +1,50 @@
 import 'dart:async';
-
 import 'dart:html';
 
 class CountdownTimer {
-  DateTime _timerDuration;
+  Duration durationOfTimer;
+  int durationSeconds;
 
-  SpanElement minute;
+  SpanElement minutes;
   SpanElement seconds;
-
-  int interval = 1;
 
   Timer timer;
 
-  CountdownTimer(this.minute, this.seconds);
+  CountdownTimer(this.minutes, this.seconds, this.durationOfTimer) {
+    durationSeconds = durationOfTimer.inSeconds;
+  }
 
-  createTimer(Duration duration) {
-    if (timer != null) {
-      timer.cancel();
-      timer = null;
+  startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (Timer t) => handleTimer());
+  }
+
+  pauseTimer() {
+    timer.cancel();
+  }
+
+  playTimer() {
+    startTimer();
+  }
+
+  handleTimer() {
+    durationSeconds--;
+    if (durationSeconds > 0) {
+      setTime();
     }
-    _timerDuration = new DateTime(0).add(duration);
-    timer = Timer.periodic(Duration(seconds: 1), (Timer t) => handleTimer(t));
   }
 
-  handleTimer(Timer timer) {
-    //create new date time
-    var newDuration = _timerDuration.subtract(Duration(seconds: interval));
+  setTime() {
+    var minute = ((durationSeconds / 60).floor());
+    var second = (durationSeconds - (minute * 60));
 
-    //update minutes and seconds
-    minute.text = newDuration.minute.toString();
-    seconds.text = newDuration.second.toString();
+    var minuteStr = minute.toString();
+    var secondStr = second.toString();
 
-    interval++;
+    if (second < 10) {
+      secondStr = '0$secondStr';
+    }
+
+    minutes.text = minuteStr;
+    seconds.text = secondStr;
   }
-
-  // updateTimer(String minutes, String seconds) {
-  //   minutes = minutes;
-  //   seconds = seconds;
-  // }
-
-  // _handleTimer() {
-  //   print('hit');
-  //   // _timerDuration.subtract(Duration(seconds: interval));
-  //   // minute = _timerDuration.minute.toString();
-  //   // seconds = _timerDuration.second.toString();
-  //   // interval++;
-  // }
 }
--- web/main.dart
+++ web/main.dart
@@ -28,30 +28,31 @@ void main() {
   stopBtn.onClick.listen(stopBtnClickHandler);
   resetBtn.onClick.listen(resetBtnClickHandler);
 
-  timer = new CountdownTimer(timerMinutes, timerSeconds);
+  timer = new CountdownTimer(
+      timerMinutes, timerSeconds, Duration(minutes: 25, seconds: 15));
 }
 
 pomodoroBtnClickHandler(MouseEvent event) {
-  timer.createTimer(Duration(minutes: 25));
+  timer.startTimer();
   print('pomodoroclicked1');
 }
 
 shortBreakBtnClickHandler(MouseEvent event) {
-  timer.createTimer(Duration(minutes: 5));
+  // timer.createTimer(Duration(minutes: 5));
   print('shortbreakclicked');
 }
 
 longBreakBtnClickHandler(MouseEvent event) {
-  timer.createTimer(Duration(minutes: 10));
+  // timer.createTimer(Duration(minutes: 10));
   print('longbreakclicked');
 }
 
 playBtnClickHandler(MouseEvent event) {
-  print('playbtnclicked');
+  timer.playTimer();
 }
 
 stopBtnClickHandler(MouseEvent event) {
-  print('stopbtnclicked');
+  timer.pauseTimer();
 }
 
 resetBtnClickHandler(MouseEvent event) {
